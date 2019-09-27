7960419bab5013b091dc82d6b08e9b15403de36c
Joseph Hentges <joehentgesbusiness@gmail.com>
2019-09-26 22:54:36 -0500

    Couple changes

--- apps/User/models/User.js
+++ apps/User/models/User.js
@@ -1,18 +1,41 @@
+/**
+ * Author
+ * Joseph Hentges
+ * September 2019
+ * https://joeyhentges.com
+ * 
+ * This is the model of the mongoose database entry.
+ */
+
+ // libraries, and getting the database connections
 const { Schema } = require('mongoose');
 const { dbs } = require('../../configs');
 
 const schema = new Schema({
+  /*
+   OAuth 2.0 Varaibles
+    - the name of the authentication location - Google, Facebook, etc...
+    - the access token for the account - hashed
+  */
+  authenticator: String,
+  accessToken: String,
+  // if signing in with software server
+  password: String,  
+  // my variables they set on first sign in
+  firstName: String,
+  lastName: String,
+  username: String,
+  email: String,
+  phone: String,
+  // some account checking variables
   locked: Boolean,
   disabled: Boolean,
   accountVerified: Boolean,
-  email: String,
-  password: String,
-  username: String,
-  firstName: String,
-  lastName: String,
+  // dates
   dateCreated: Date,
   dateModified: Date,
   dateLastLoggedIn: Date,
+  // objects
   securityQuestions: {
     questionOne: {
       question: String,
@@ -27,7 +50,10 @@ const schema = new Schema({
       answer: String,
     },
   },
-  logs: [{}],
+  logs: [{
+    time: String,
+    message: String,
+  }],
 });
 
 module.exports.User = dbs.db1().model('User', schema);
